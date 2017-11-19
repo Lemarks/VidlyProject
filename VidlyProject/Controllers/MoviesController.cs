9a5fe50d52639f28cc7fbcde5b726d1bcb70e2fb
Les O <lemark@live.com>
2017-11-19 17:36:51 -0500

    Movies and Customer DB and Webside updated with seed data, overriden conventions and queries all round

--- VidlyProject/Controllers/MoviesController.cs
+++ VidlyProject/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using VidlyProject.Models;
 
@@ -6,6 +7,7 @@ namespace VidlyProject.Controllers
 {
     public class MoviesController : Controller
     {
+        //! Test Code
         // GET: Movies/Random
         //public ActionResult Random()
         //{
@@ -58,21 +60,34 @@ namespace VidlyProject.Controllers
         //    return Content(year + "/" + month);
         //}
 
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
 
         }
 
-        private static IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie {Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Mortal Kombat"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
     }
 }
