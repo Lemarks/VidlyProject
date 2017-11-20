3c0911abc742d230487ef64f4bbaf871da77896f
Les O <lemark@live.com>
2017-11-20 02:53:21 -0500

    Added client-side,server-side, custom validation and anti-forgery tokens to Customer and Movie

--- VidlyProject/Controllers/CustomersController.cs
+++ VidlyProject/Controllers/CustomersController.cs
@@ -26,6 +26,7 @@ namespace VidlyProject.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -33,8 +34,20 @@ namespace VidlyProject.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
